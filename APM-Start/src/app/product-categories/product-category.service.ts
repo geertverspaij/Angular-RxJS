ef27df1fd908fba9fbf5cd2ca640dd9766bc533e
geert verspaij <geertverspaij@hotmail.nl>
2022-04-12 13:55:17 +0200

    H7 Combining data streams

--- APM-Start/src/app/product-categories/product-category.service.ts
+++ APM-Start/src/app/product-categories/product-category.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, tap, catchError } from 'rxjs';
+import { ProductCategory } from './product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,11 @@ import { throwError, Observable } from 'rxjs';
 export class ProductCategoryService {
   private productCategoriesUrl = 'api/productCategories';
 
+  productCategories$ = this.http.get<ProductCategory[]> (this.productCategoriesUrl)
+    .pipe(
+      tap(data => console.log(`categories`, JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   constructor(private http: HttpClient) { }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
